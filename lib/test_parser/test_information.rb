288f070160aa8f6d5fb252cce6fc4a43ff92bd5f
Juan Manuel Barreneche <snipperme@gmail.com>
2011-02-20 22:44:52 -0300

    Damn typo

--- lib/test_parser/test_information.rb
+++ lib/test_parser/test_information.rb
@@ -1,13 +1,15 @@
 module TestParser
   class TestInformation < Struct.new(:type, :identification, :file, :extras)
+
     def snippet
       source_code = SourceCode.for(file)
-      case test.type
+      case type
       when :rspec2
         source_code.extract_code_from_line(extras[:line_number])
       when :minitest
         source_code.extract_method(extras[:method_name])
       end
     end
+
   end
 end
