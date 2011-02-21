5cc2af4489c5c4fd7dc103bcf98bab417dbedfa6
Juan Manuel Barreneche <snipperme@gmail.com>
2011-02-20 22:32:17 -0300

    Added snippet to sourcecode

--- lib/test_parser/test_information.rb
+++ lib/test_parser/test_information.rb
@@ -1,3 +1,13 @@
 module TestParser
-  TestInformation = Struct.new(:type, :identification, :file, :extras)
+  class TestInformation < Struct.new(:type, :identification, :file, :extras)
+    def snippet
+      source_code = SourceCode.for(file)
+      case test.type
+      when :rspec2
+        source_code.extract_code_from_line(extras[:line_number])
+      when :minitest
+        source_code.extract_method(extras[:method_name])
+      end
+    end
+  end
 end
